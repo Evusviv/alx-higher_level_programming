f639bec334646496e7689df1dbf6da536de2436f
Evusviv <99492131+Evusviv@users.noreply.github.com>
2022-09-24 05:02:51 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
-             as response:
+            as response:
         html = response.read()
     print("Body response:")
     print('\t- type: {}'.format(type(html)))
